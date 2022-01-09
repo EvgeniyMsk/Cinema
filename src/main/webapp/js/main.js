e709034c7ec5dca259be579ff65dd7fc2bd742a2
Qsymond <logvinov.evgeniy@ya.ru>
2022-01-09 22:47:44 +0300

    Cinema готов.

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -8,10 +8,6 @@ var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
 var stompClient = null;
 var username = null;
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
 
 function loadMessages() {
 var x = new XMLHttpRequest();
